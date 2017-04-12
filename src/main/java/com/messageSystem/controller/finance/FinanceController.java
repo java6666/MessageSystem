0de8613c6d8384c7087f621a64eee82c08fe118a
张超 <737255782@qq.com>
2017-04-12 19:59:23 +0800

    财务

--- src/main/java/com/messageSystem/controller/finance/FinanceController.java
+++ src/main/java/com/messageSystem/controller/finance/FinanceController.java
@@ -1,8 +1,15 @@
 package com.messageSystem.controller.finance;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by admin on 2017/4/12.
+ * 财务层
  * @author 张超
  */
+@Controller
 public class FinanceController {
+
+
+
 }
